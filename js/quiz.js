403511315abe3c85c236f430125a62f5bfcc84ed
1835207 <87455112+1835207@users.noreply.github.com>
2021-07-24 01:28:30 +0900

    Add files via upload

---
+++ js/quiz.js
@@ -0,0 +1,13 @@
+function quiz(choice) {
+    var message = "";
+
+    if (choice == 0) {
+        message = "不正解";
+    } else if (choice == 1) {
+        message = "不正解";
+    } else if (choice == 2) {
+        message = "正解";
+    } 
+
+    alert(message);
+}
